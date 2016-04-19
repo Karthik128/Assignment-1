c77b74f1b73ad0edabb4f60003c854e8733c70a5
Karthik128 <dr.m.karthiik@gmail.com>
2016-04-19 17:54:39 +0530

    All Assignments

---
+++ AcadGuild/src/Assignment1.java
@@ -0,0 +1,19 @@
+
+public class Assignment1 {
+
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+Integer x=1;
+Integer y=27;
+Integer z= x+y;
+
+
+
+System.out.println(z);
+    
+	}
+
+}
